41c1dc0639f8a0408992ef0106419bb195f5bc63
Sarath Krishnan <sarath7916@gmail.com>
2024-01-16 18:46:32 +0530

    STACK ADDED

--- sorting_algorithms/insertion_sort.dart
+++ sorting_algorithms/insertion_sort.dart
@@ -1,5 +1,23 @@
 List<int> insertionSort(array) {
+  for (var i = 1; i < array.length; i++) {
+    int current = array[i];
+    int prev = i - 1;
+    while (prev >= 0 && array[prev] > current) {
+      array[prev + 1] = array[prev];
+      prev--;
+    }
 
-  
+    array[prev + 1] = current;
+  }
+
   return array;
+
+}
+
+void main() {
+  List<int> array = [2, 8, 9, 6, 3, 10, 11, 1, 1, 7];
+  print("original array");
+  print(array);
+  print("sorted array");
+  print(insertionSort(array));
 }
